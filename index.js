d7c06fa531b85100e5d4168eb107bd04a34e1d14
Chris Rudel <chris@DESKTOP-GHK7GBU.localdomain>
2023-04-11 20:37:25 -0400

    created api that calls ffz and stores result in variable. calls api every day at 3am in prod. need to deploy to heroku

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const axios = require("axios");
+const cron = require("node-cron");
+
+require('dotenv').config
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+let data = null;
+
+// Define a cron job to run every day at 3am
+const cronSchedule = process.env.NODE_ENV === 'production' ? '0 3 * * *' : '*/2 * * * *';
+console.log(cronSchedule);
+cron.schedule(cronSchedule, async () => {
+  console.log("called cron");
+  try {
+    const responses = await getAll();
+    data = response;
+    console.log('Data updated successfully');
+  } catch(e){
+    res.status(400).json(e);
+  }
+});
+
+// Route to get all emotes
+app.get("/data", async (req, res) => {
+  if (!data) {
+    try{
+      const responses = await getAll();
+      data = responses
+    }catch(e){
+      res.status(400).json(e);
+    }
+    
+  }
+  res.json(data);
+});
+
+async function getAll() {
+    // throw{
+    //   status: 400,
+    //   error: "Testing an error"
+    // }
+    console.log("called getAll");
+    const responses = {};
+    for (let i = 0; i < 3; i++) {
+      let apiCall = await axios.get(
+        `https://api.frankerfacez.com/v1/emotes?sensitive=false&sort=count-desc&page=${
+          i + 1
+        }&per_page=200`
+      );
+      let { data } = apiCall;
+      let { _pages, _total, emoticons } = data;
+      emoticons.map(({ id, name, usage_count, urls }) => {
+        if (!(name in responses)) {
+          responses[name] = { id: id, usage_count: usage_count, urls: urls };
+        }
+      });
+    }
+    
+  return responses;
+}
+
+// Start the server
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
