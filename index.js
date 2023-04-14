8100d6785930638b11ebd9b028f14d0c89dcc31b
Christopher Rudel <chris.rudel@gmail.com>
2023-04-14 17:32:06 -0400

    the emote name might not be readable when decoding api call in swift. adding the name to the object just in case

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const cron = require("node-cron");
 
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -12,7 +12,6 @@ let data = null;
 
 // Define a middleware function to check for a valid API key
 const authenticateAPIKey = (req, res, next) => {
-  
   const apiKey = req.query.apiKey;
   //console.log(API_KEY);
   if (apiKey === API_KEY) {
@@ -27,15 +26,16 @@ const authenticateAPIKey = (req, res, next) => {
 // to check heroku logs run command heroku logs --tail
 
 // Define a cron job to run every day at 3am in prod. job runs every min in nonprod
-const cronSchedule = process.env.NODE_ENV === 'production' ? '0 3 * * *' : '*/1 * * * *';
+const cronSchedule =
+  process.env.NODE_ENV === "production" ? "0 3 * * *" : "*/1 * * * *";
 console.log(cronSchedule);
 cron.schedule(cronSchedule, async () => {
   try {
     const responses = await getAll();
     data = responses;
-    console.log('Data updated successfully');
+    console.log("Data updated successfully");
     //console.log(data);
-  } catch(e){
+  } catch (e) {
     res.status(400).json(e);
   }
 });
@@ -43,13 +43,12 @@ cron.schedule(cronSchedule, async () => {
 // Route to get all emotes
 app.get("/data", authenticateAPIKey, async (req, res) => {
   if (!data) {
-    try{
+    try {
       const responses = await getAll();
-      data = responses
-    }catch(e){
+      data = responses;
+    } catch (e) {
       res.status(400).json(e);
     }
-    
   }
   res.json(data);
 });
@@ -62,27 +61,32 @@ app.get("/data", authenticateAPIKey, async (req, res) => {
 // })
 
 async function getAll() {
-    // throw{
-    //   status: 400,
-    //   error: "Testing an error"
-    // }
-    console.log("called getAll");
-    const responses = {};
-    for (let i = 0; i < 3; i++) {
-      let apiCall = await axios.get(
-        `https://api.frankerfacez.com/v1/emotes?sensitive=false&sort=count-desc&page=${
-          i + 1
-        }&per_page=200`
-      );
-      let { data } = apiCall;
-      let { _pages, _total, emoticons } = data;
-      emoticons.map(({ id, name, usage_count, urls }) => {
-        if (!(name in responses)) {
-          responses[name] = { id: id, usage_count: usage_count, urls: urls };
-        }
-      });
-    }
-    
+  // throw{
+  //   status: 400,
+  //   error: "Testing an error"
+  // }
+  console.log("called getAll");
+  const responses = {};
+  for (let i = 0; i < 3; i++) {
+    let apiCall = await axios.get(
+      `https://api.frankerfacez.com/v1/emotes?sensitive=false&sort=count-desc&page=${
+        i + 1
+      }&per_page=200`
+    );
+    let { data } = apiCall;
+    let { _pages, _total, emoticons } = data;
+    emoticons.map(({ id, name, usage_count, urls }) => {
+      if (!(name in responses)) {
+        responses[name] = {
+          name: name,
+          id: id,
+          usage_count: usage_count,
+          urls: urls,
+        };
+      }
+    });
+  }
+
   return responses;
 }
 
